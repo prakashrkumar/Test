31a5e270044df4eaba24dbd638d2d1fe44565cfb
prakash_kumar <prakashr17032000@gmail.com>
2026-04-29 18:07:34 +0530

    4th commit

--- first.java
+++ first.java
@@ -1,5 +1,8 @@
 public class first {
     public static void main(String[] args) {
         System.out.println("Hello, World! hello");
+        System.out.println("kay cjay rahaha hai");
+        int a=8;
+        System.out.println(a);
     }
 }
